a5cfefd00cab133ff72bc499e3b82fbc50bca071
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-09-26 00:44:47 -0300

    Created startup card UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,6 +7,19 @@ export default async function Home({
   searchParams: Promise<{ query: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _id: 1,
+      title: "We Robots",
+      author: { _id: 1, name: "Adrian" },
+      description: "This is a description",
+      image: "https://images.unsplash.com/photo-1667986292516-f27450ae75a9",
+      category: "Robots",
+      _createdAt: new Date(),
+      views: 15,
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -17,6 +31,22 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
