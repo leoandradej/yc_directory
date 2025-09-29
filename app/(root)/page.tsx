aff67a885fd9ae64f0c1a46105f8614b06323cea
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-09-28 22:53:02 -0300

    Enabled search bar to filter results

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,9 +9,10 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-
+  const params = { search: query || null };
   const { data: posts } = await sanityFetch({
     query: STARTUPS_QUERY,
+    params,
   });
 
   return (
